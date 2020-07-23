4d8799005b5d9a16d9cd648638aa29d0bd8242b0
Douglas <jdps@cin.ufpe.br>
2020-07-23 11:31:08 -0300

    changes current formation randomly every 5 minutes

--- Coach.py
+++ Coach.py
@@ -1,33 +1,30 @@
 import socket
-
+import time
 import struct
-from torch import nn
+import random
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serv.bind(('0.0.0.0', 8084))
 
 
-#serv.listen(5)
+NUM_FORMACOES = 3 #number of valid options for (formations/players/behaviors/We need a better name for this....)
+END_GAME = 5*60 #seconds until next change
+START = (END_GAME + 1)*(-1) + time.time()
 
 
 
 try:
-
     while True:
-        #d, client = serv.recvfrom(4096)
-        #if not d: break
-        aux = input("escreve um inteiro para a zona 0, 1 ou 2")
-        out_str = struct.pack('i',int(aux))
-        #for i in range(0,2):
-        #    print(client, struct.unpack_from('f',out_str,4*i)[0])
-        serv.sendto(out_str, ('0.0.0.0', 4097))
-    #conn.close()
-    print ('client disconnected')
+        if ((time.time() - START) > END_GAME):
+            print("mudando a formacao para")
+            aux = (random.randint(0,NUM_FORMACOES))
+            print(aux)
+            out_str = struct.pack('i',int(aux))
+            serv.sendto(out_str, ('0.0.0.0', 4097))
+            START = time.time()
+			
+
 
 except Exception as e: 
     print(e)
-    print("Close Server!")
-    #conn.close()
     serv.close()
-
-        

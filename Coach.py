52287a4a5bab62e78ae74bcdddb27da1d51f60ec
Douglas <jdps@cin.ufpe.br>
2020-07-26 21:19:42 -0300

    default value defined with argparse

--- Coach.py
+++ Coach.py
@@ -2,6 +2,7 @@ import socket
 import time
 import struct
 import random
+import argparse
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 serv.bind(('0.0.0.0', 8084))
@@ -12,13 +13,26 @@ END_GAME = 5*60 #seconds until next change
 START = (END_GAME + 1)*(-1) + time.time()
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--default", help="use --default if you want to send a default value, instead of a random one")
+args = parser.parse_args()
+
+
 
 try:
     while True:
         if ((time.time() - START) > END_GAME):
-            print("mudando a formacao para")
-            aux = (random.randint(0,NUM_FORMACOES))
-            print(aux)
+            if args.default:
+                print("default value turned on:")
+                print(args.default)
+                aux = args.default
+                out_str = struct.pack('i',int(aux))
+                serv.sendto(out_str, ('0.0.0.0', 4097))
+                break
+            else:
+                print("mudando a formacao para")
+                aux = (random.randint(0,NUM_FORMACOES))
+                print(aux)
             out_str = struct.pack('i',int(aux))
             serv.sendto(out_str, ('0.0.0.0', 4097))
             START = time.time()

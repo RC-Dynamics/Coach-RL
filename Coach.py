4501539361c60a4a4a602bda200b466808a96b6a
Douglas <jdps@cin.ufpe.br>
2020-08-05 13:29:47 -0300

    integrando os calculos

--- Coach.py
+++ Coach.py
@@ -5,85 +5,59 @@ import random
 import argparse
 
 from fira_parser import *
-
-fira = FiraParser()
-fira.start()
-
-serv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serv.bind(('0.0.0.0', 8084))
+from Game import Stats, History
 
 
-NUM_FORMACOES = 3 #number of valid options for (formations/players/behaviors/We need a better name for this....)
-END_GAME = 5*60 #seconds until next change
-START = (END_GAME + 1)*(-1) + time.time()
+def main():
+    fira = FiraParser()
+    fira.start()
+
+    serv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serv.bind(('0.0.0.0', 8084))
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--default", help="use --default if you want to send a default value, instead of a random one")
-args = parser.parse_args()
+    NUM_FORMACOES = 3 #number of valid options for (formations/players/behaviors/We need a better name for this....)
+    END_GAME = 5*60 #seconds until next change
+    START = (END_GAME + 1)*(-1) + time.time()
 
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--default", help="use --default if you want to send a default value, instead of a random one")
+    args = parser.parse_args()
 
-try:
-    while True:
-        if ((time.time() - START) > END_GAME):
-            if args.default:
-                print("default value turned on:")
-                print(args.default)
-                aux = args.default
+    try:
+        history = History(100,3,3)
+        
+        while True:
+            if ((time.time() - START) > END_GAME):
+                if args.default:
+                    print("default value turned on:")
+                    print(args.default)
+                    aux = args.default
+                    out_str = struct.pack('i',int(aux))
+                    serv.sendto(out_str, ('0.0.0.0', 4098))
+                    break
+                else:
+                    print("mudando a formacao para")
+                    aux = (random.randint(0,NUM_FORMACOES))
+                    print(aux)
                 out_str = struct.pack('i',int(aux))
                 serv.sendto(out_str, ('0.0.0.0', 4098))
-                break
-            else:
-                print("mudando a formacao para")
-                aux = (random.randint(0,NUM_FORMACOES))
-                print(aux)
-            out_str = struct.pack('i',int(aux))
-            serv.sendto(out_str, ('0.0.0.0', 4098))
-            START = time.time()
+                START = time.time()
 
-        #Recebendo dados do simulador...
-        data = fira.receive()
-        data.step #time
-        data.goals_yellow #gols yellow
-        data.goals_blue #gols blue
+            #Recebendo dados do simulador...
+            data = fira.receive()
+            history.update(data)
+            stats = Stats(data)
+            bp = stats.get_bp(data)
+            ctrl = stats.get_controll(history)
+            DDB = stats.get_DDB(data)
+            phi, c = stats.get_param(history)
 
-        # Ball
-        ball = data.frame.ball
-        ball.x 
-        ball.y
-        ball.vx
-        ball.vy
-        
+    except Exception as e: 
+        print(e)
+        serv.close()
+        fira.stop()
 
-        # Robots Yellow
-        for robot in data.frame.robots_yellow:
-
-            robot.x
-            robot.y
-            robot.orientation
-            robot.vx
-            robot.vy
-            robot.vorientation
-            
-
-        # Robots Blue
-        for robot in data.frame.robots_blue:
-
-            robot.x
-            robot.y
-            robot.orientation
-            robot.vx
-            robot.vy
-            robot.vorientation
-            
-
-        print(data.frame.ball.x)
-        print(data.frame.ball.y)
-			
-
-
-except Exception as e: 
-    print(e)
-    serv.close()
-    fira.stop()
+if __name__ == "__main__":
+    main()

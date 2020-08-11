f95d6edf78e52645c52a4d2d12659425c3221bd6
goncamateus <mgm4@cin.ufpe.br>
2020-08-11 13:22:24 -0300

    implements some env basics functions (not complete)

--- gym_coach_vss/coach_env.py
+++ gym_coach_vss/coach_env.py
@@ -1,1 +1,79 @@
+import os
+import socket
+import subprocess
+
 import gym
+from gym.spaces import Box
+
+from gym_coach_vss.fira_parser import FiraParser
+from gym_coach_vss.Game import History, Stats
+
+BIN_PATH = '/'.join(os.path.abspath(__file__).split('/')
+                    [:-1]) + '/bin/'
+
+
+class CoachEnv(gym.Env):
+
+    def __init__(self, addr='224.5.23.2', fira_port=10020,
+                 sw_port=8084, max_history=10, fast_mode=True,
+                 render=False, sim_path=None, versus='determistic'):
+
+        super(CoachEnv, self).__init__()
+        self.addr = addr
+        self.sw_port = sw_port
+        self.fira_port = fira_port
+        self.fira = None
+        self.sw_conn = None
+        self.fast_mode = fast_mode
+        self.do_render = render
+        self.sim_path = sim_path
+        self.history = History(max_history)
+        self.agent_blue_process = None
+        self.agent_yellow_process = None
+        self.versus = versus
+        # self.observation_space = Box(low=-1.0, high=1.0, shape=())
+
+    def start_agents(self):
+        command_blue = BIN_PATH + 'VSSL_blue'
+        command_yellow = BIN_PATH + 'VSSL_yellow'
+        self.agent_blue_process = subprocess.Popen(command_blue)
+        self.agent_yellow_process = subprocess.Popen(command_yellow)
+        self.sw_conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sw_conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sw_conn.setsockopt(
+            socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 128)
+        self.sw_conn.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1)
+        self.sw_conn.bind(('0.0.0.0', 8084))
+
+    def stop_agents(self):
+        self.agent_blue_process.terminate()
+        self.agent_blue_process.wait()
+        self.agent_yellow_process.terminate()
+        self.agent_yellow_process.wait()
+        self.sw_conn.close()
+        self.sw_conn = None
+
+    def start(self):
+        if self.fira is None:
+            self.fira = FiraParser('224.5.23.2', port=self.fira_port,
+                                   fast_mode=self.fast_mode,
+                                   render=self.render,
+                                   sim_path=self.sim_path)
+        self.fira.start()
+        self.start_agents()
+
+    def stop(self):
+        self.stop_agents()
+        self.fira.stop()
+
+    def _receive_state(self):
+        return 1
+
+    def reset(self):
+        self.stop()
+        self.start()
+        state = self._receive_state()
+        return state
+
+    def step(self, action):
+        pass
--- gym_coach_vss/fira_parser.py
+++ gym_coach_vss/fira_parser.py
@@ -1,4 +1,6 @@
 import math
+import os
+import socket
 import subprocess
 
 from gym_coach_vss.fira_client import FiraClient
@@ -6,16 +8,31 @@ from gym_coach_vss.fira_client import FiraClient
 
 class FiraParser(object):
 
-    def __init__(self, ip='224.5.23.2', port=10020):
+    def __init__(self, ip='224.5.23.2', port=10020,
+                 fast_mode=True, render=False, sim_path=None):
         # -- Connection
         self.ip = ip
         self.port = port
         self.conn = FiraClient(ip=ip, port=self.port)
+        self.com_socket = None
+        self.address = None
+        self.is_running = False
+        self.process = None
+        self.goals_left = 0
+        self.goals_right = 0
+        self.fast_mode = fast_mode
+        self.render = render
+        if sim_path is not None:
+            self.simulator_path = sim_path
+        else:
+            home = '/home/' + os.getenv('user')
+            self.simulator_path = home + 'FIRASim/bin/FIRASim'
 
     # Simulation methods
     # ----------------------------
 
     def start(self):
+        self._start_simulation()
         self._connect()
         self.is_running = True
         self.goals_left = 0
@@ -24,73 +41,26 @@ class FiraParser(object):
     def stop(self):
         self.is_running = False
         self._disconnect()
+        self._stop_simulation()
+
+    def _start_simulation(self):
+        command = [self.simulator_path]
+        if not self.render:
+            command.append('-H')
+        if self.fast_mode:
+            command.append('--xlr8')
+        self.process = subprocess.Popen(command)
+
+    def _stop_simulation(self):
+        self.process.terminate()
+        self.process.wait()
 
     def reset(self):
         self.stop()
         self.start()
 
-    # Network methods
-    # ----------------------------
-    def convert_ssl_to_sim_coord(self, pose, v_pose):
-        width = 1.3/2.0
-        lenght = (1.5/2.0) + 0.1
-        '''if(self.is_team_yellow):
-            pose.x = (lenght - pose.x)*100
-            pose.y = (width + pose.y)*100
-            pose.yaw = math.pi - pose.yaw
-            v_pose.x *= -100
-            v_pose.y *= 100
-            v_pose.yaw = v_pose.yaw
-        else:
-            pose.x = (lenght+pose.x)*100
-            pose.y = (width - pose.y)*100
-            pose.yaw = -pose.yaw
-            v_pose.x *= 100
-            v_pose.y *= -100
-            v_pose.yaw = v_pose.yaw'''
-
     def receive(self):
         data = self.conn.receive()
-        '''state = Global_State()
-        state.time = data.step
-        state.goals_yellow = data.goals_yellow
-        state.goals_blue = data.goals_blue
-
-        # Ball
-        ball = data.frame.ball
-        ball_state = state.balls.add()
-        ball_state.pose.x = ball.x
-        ball_state.pose.y = ball.y
-        ball_state.v_pose.x = ball.vx
-        ball_state.v_pose.y = ball.vy
-        self.convert_ssl_to_sim_coord(ball_state.pose, ball_state.v_pose)
-
-        # Robots Yellow
-        for robot in data.frame.robots_yellow:
-            robot_state = state.robots_yellow.add()
-            robot_state.pose.x = robot.x
-            robot_state.pose.y = robot.y
-            robot_state.pose.yaw = robot.orientation
-            robot_state.v_pose.x = robot.vx
-            robot_state.v_pose.y = robot.vy
-            robot_state.v_pose.yaw = robot.vorientation
-            self.convert_ssl_to_sim_coord(robot_state.pose,
-                                          robot_state.v_pose)
-
-        # Robots Blue
-        for robot in data.frame.robots_blue:
-            robot_state = state.robots_blue.add()
-            robot_state.pose.x = robot.x
-            robot_state.pose.y = robot.y
-            robot_state.pose.yaw = robot.orientation
-            robot_state.v_pose.x = robot.vx
-            robot_state.v_pose.y = robot.vy
-            robot_state.v_pose.yaw = robot.vorientation
-            self.convert_ssl_to_sim_coord(robot_state.pose,
-                                          robot_state.v_pose)
-        # print(state.robots_blue[0].v_pose)'''
-        # print(data.frame.ball.x)
-        # print(data.frame.ball.y)
         return data
 
     def _disconnect(self):

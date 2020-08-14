d083a348d1b26840387cc1f6ef738e563639e82e
Lucas Dias <ldm@cin.ufpe.br>
2020-08-14 13:49:56 -0300

    Adding ball gradient in the reward computing

--- gym_coach_vss/coach_env.py
+++ gym_coach_vss/coach_env.py
@@ -3,6 +3,7 @@ import socket
 import struct
 import subprocess
 import time
+import math
 
 import gym
 import numpy as np
@@ -13,6 +14,10 @@ from gym_coach_vss.Game import History, Stats
 
 BIN_PATH = '/'.join(os.path.abspath(__file__).split('/')
                     [:-1]) + '/bin/'
+
+
+w_grad_ball_potential = (0.08, 1)
+
 
 
 class CoachEnv(gym.Env):
@@ -41,10 +46,12 @@ class CoachEnv(gym.Env):
         self.goal_prev_yellow = 0
         self.goal_prev_blue = 0
         self.is_discrete = is_discrete
+        self.prev_time = 0.0
         if not self.is_discrete:
             self.observation_space = Box(
                 low=-1.0, high=1.0, shape=(qtde_steps, 29), dtype=np.float32)
         self.action_space = Discrete(27)
+
 
     def start_agents(self):
         if self.versus == 'determistic':
@@ -57,8 +64,8 @@ class CoachEnv(gym.Env):
             raise ValueError(f'No team with {self.versus} type')
         command_yellow = [BIN_PATH + 'VSSL_yellow']
         command_yellow.append('-H')
-        self.agent_blue_process = subprocess.Popen(command_blue)
-        self.agent_yellow_process = subprocess.Popen(command_yellow)
+        # self.agent_blue_process = subprocess.Popen(command_blue)
+        # self.agent_yellow_process = subprocess.Popen(command_yellow)
         time.sleep(5)
         self.sw_conn = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sw_conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -68,10 +75,10 @@ class CoachEnv(gym.Env):
         self.sw_conn.bind(('0.0.0.0', 8084))
 
     def stop_agents(self):
-        self.agent_blue_process.terminate()
-        self.agent_blue_process.wait()
-        self.agent_yellow_process.terminate()
-        self.agent_yellow_process.wait()
+        # self.agent_blue_process.terminate()
+        # self.agent_blue_process.wait()
+        # self.agent_yellow_process.terminate()
+        # self.agent_yellow_process.wait()
         self.sw_conn.close()
         self.sw_conn = None
 
@@ -109,26 +116,58 @@ class CoachEnv(gym.Env):
         state = self._receive_state(reset=True)
         return np.array(state)
 
+    def ball_potential(self, step=-1):
+        dx_d = 0 - self.history.balls[step].x  # distance to defence
+        dx_a = 170.0 - self.history.balls[step].x  # distance to attack
+        dy = 65.0 - self.history.balls[step].y
+        potential = ((-math.sqrt(dx_a ** 2 + 2 * dy ** 2)
+                      + math.sqrt(dx_d ** 2 + 2 * dy ** 2)) / 170 - 1) / 2
+
+        return potential
+
+    def bound(self, value, floor, ceil):
+        if value < floor:
+            return floor
+        elif value > ceil:
+            return ceil
+        else:
+            return value
+
+
     def compute_rewards(self):
         diff_goal_blue = self.goal_prev_blue - self.history.data.goals_blue
         diff_goal_yellow = self.history.data.goals_yellow - self.goal_prev_yellow
 
-        reward = 0
-        if diff_goal_blue < 0:
+        ball_potential = self.ball_potential()
+        prev_ball_potential = self.ball_potential((-2))
+
+        dt = self.history.time - self.prev_time
+        self.prev_time = self.history.time
+
+        if self.history.balls is not []:
+            grad_ball_potential = self.bound((ball_potential - prev_ball_potential) * 4000 / dt, -1.0,
+                                       1.0)  # (-1,1)
+        else:
+            grad_ball_potential = 0.0
+        
+        reward = 0.0
+        if diff_goal_blue < 0.0:
             print('********************GOAL BLUE*********************')
             self.goal_prev_blue = self.history.data.goals_blue
             print(
                 f'Blue {self.goal_prev_blue} vs {self.goal_prev_yellow} Yellow'
             )
-            reward += diff_goal_blue*1000
+            reward += diff_goal_blue*1000.0
 
-        if diff_goal_yellow > 0:
+        if diff_goal_yellow > 0.0:
             print('********************GOAL YELLOW*******************')
             self.goal_prev_yellow = self.history.data.goals_yellow
             print(
                 f'Blue {self.goal_prev_blue} vs {self.goal_prev_yellow} Yellow'
             )
-            reward += diff_goal_yellow*1000
+            reward += diff_goal_yellow*1000.0
+
+        reward += grad_ball_potential * w_grad_ball_potential[0]
 
         return reward
 
